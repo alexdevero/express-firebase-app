9f2b65bf8b8666a931933df92768407cc7318f94
Alex Devero <deveroalex@gmail.com>
2022-09-26 16:49:41 +0200

    Add additional express middleware deps

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,11 @@ import express, { Express } from 'express'
 import dotEnv from 'dotenv'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+// @ts-ignore
+import { xss } from 'express-xss-sanitizer'
 
 import mainRouter from '../routes/index'
 
@@ -10,7 +15,12 @@ dotEnv.config()
 const app: Express = express()
 const port: number = Number(process.env.PORT) || 3000
 
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(xss());
 app.use(cookieParser())
 
 app.use('/', mainRouter)
